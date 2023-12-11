02ce93be202102327656693608257ceacd930ad7
hzemouri <hocem.zemouri-ext@groupe-satec.com>
2023-12-11 11:29:10 +0100

    Removing old projects

--- order-service/src/main/java/fr/dovi/orderservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.dovi.orderservice.repository;
-
-import fr.dovi.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- product-service/src/main/java/fr/dovi/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.dovi.productservice.repository;
-
-import fr.dovi.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
